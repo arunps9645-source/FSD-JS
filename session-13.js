6c25506f5c8db76329f2db140f12b918aac0f4bf
arunps9645 <arunps9645@gmail.com>
2026-09-25 20:56:05 +0530

    session-13.js

---
+++ session-13.js
@@ -0,0 +1,158 @@
+// console.log(hello);
+// let name = "arun";
+// console.log("Name=",name);
+// console.log(typeof name)
+// let age ="35";
+// console.log(typeof age);
+// let age =35;
+// console.log(typeof age);
+
+// TYPE CONVERSION
+// let age = Number("34");
+
+// let city = undefined;
+
+// console.log(typeof age);
+// console.log(Number(true));
+//  console.log(Number(false));
+
+//  console.log( typeof String(true));   
+//  console.log(string(undefined));
+//  console.log(Boolean("11"));
+//
+
+// type coercion , auto
+//console.log("10"+2); // this is converted to string  (this is two things in colom string and number )
+//console.log(10+2); 
+//console.log("10"+2+3); // anser is 1023  is fully converted to string (we add string and integer )
+//console.log("10"-2); // answer is 8 we use adition (+) operation it convert string and we use subtraction(-) that convert to number
+//console.log("10"*2); // answer is 20 this also is convert to number (multiplication)
+
+
+//ARITHMETIC OPERATION = +,-,*,/,%
+//console.log(10 + 10); //  20
+//console.log(10 % 2);// anser is 0
+//console.log(10*2);// 20
+//console.log(10**3); // 1000
+
+// COMPARISON OPERATION - >,<,>=,<=,==,!==
+//console.log(10 >2); // true
+//console.log(10 <2); // faalse
+//console.log(10>=10); // true
+//console.log(10==10); // true
+//console.log(10===10);// true
+//console.log(10 =="10") // true
+// console.log(10 ==="10")// false because we use (===) they look the type also
+
+//ASIGMENT OPERATOR
+// let score = 90;
+// score -= 5;
+// console.log(score);
+//-------------------------
+// //LOGICAAL OPERATOR
+// //&&,||, !
+
+// console.log(true && false)
+// console.log(false && false)
+// console.log(true && true)
+
+// console.log(true || false)
+// console.log(false || false)
+
+// console.log(10>2 && 5<2); // false
+// console.log(10>2 ||5<2); // true
+
+// console.log(! true); // false  (oposite )
+
+//-----------------------
+
+// STATEMENT
+
+// if 
+
+//     let score = 56;
+// if (score > 40) {
+//          console.log("passed")
+
+//}
+//----------------
+// if else
+
+// let score = 30;
+// if (score > 40){
+//     console.log("passed")
+// }
+// else{
+//     console.log("failed")
+// }
+// console.log("all the best")
+//--------------------
+
+//if else if 
+
+// let score = 98;
+// if (score >=90){
+//     console.log("A+");
+// }
+// else if (score >=80){
+//     console.log("A");
+// }
+
+// else if (score >=70){
+//     console.log("B");
+// }
+// else {
+//     console.log("E")
+// }
+// console.log("all the best")
+//------------------------
+//  Switch statement
+
+// let month = 1;
+// switch (month) {
+//     case 1:
+//         console.log("jan");
+//         break;
+//          case 2:
+//         console.log("feb");
+//         break;
+//          case 3:
+//         console.log("mar");
+//         break;
+//          case 4:
+//         console.log("apr");
+//         break;
+//          case 5:
+//         console.log("may");
+//         break;
+//         default:
+//             console.log("invalid")
+// }
+// console.log("all the best")
+
+// let score = 94;
+// switch (score >0) {   // condition must need in switch (use same comparision compact)
+//     case score >=90:
+//         console.log("A+");
+//         break;
+//          case score >=80:
+//         console.log("b");
+//         break;
+//          case score >=70:
+//         console.log("c");
+//         break;
+//         default:
+//             console.log("invalid")
+// }
+// console.log("all the best")
+
+// let weekDay = "sunday";
+// switch(weekDay){
+//     case "monday" :console.log("special day")
+//     break;
+//     case "friday" :console.log("happy friday")
+//     default:
+//         console.log("nice day")
+// }
+
+
